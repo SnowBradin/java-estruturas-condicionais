d67160018c05badbd9c45ec3bcc73e2220352ff5
Bradin <danbleeding123@gmail.com>
2024-06-28 21:31:39 -0300

    if-else-test

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,11 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args)  {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 26.0;
 
-        if(valorSolicitado <saldo)
+        if(valorSolicitado <saldo){
         saldo = saldo - valorSolicitado;
+        System.out.println("Novo Saldo " + saldo);
+        } //se existir mais de uma linha no "if", coloque-o em um bloco {}
+        else
+        System.out.println("Saldo Insuficiente");
 
-        System.out.println(saldo);
+        
     }
 }
